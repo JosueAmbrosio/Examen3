3aba4c206487c18d5b84a37d6666221040a29fc4
JosueAmbrosio <josue.ambrosio@tecsup.edu.pe>
2024-11-07 10:01:55 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { ClienteComponent } from './components/cliente/cliente.component';
+import { ServicioComponent } from './components/servicio/servicio.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { MarcaComponent } from './components/marca/marca.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { CountsComponent } from './components/counts/counts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'nosotros', component:NosotrosComponent },
+    { path: 'cliente', component: ClienteComponent },
+    {
+        path: 'servicios', component: ServicioComponent, children: [
+          { path: '', component: CountsComponent }, 
+          { path: 'counts', component: CountsComponent }
+        ]
+      },    { path: 'productos', component: ProductosComponent},
+    { path:'marca', component: MarcaComponent},
+    { path: 'contacto', component: ContactoComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,39 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { NavmenuComponent } from './components/navmenu/navmenu.component';
+import { ClienteComponent } from './components/cliente/cliente.component';
+import { CountsComponent } from './components/counts/counts.component';
+import { ServicioComponent } from './components/servicio/servicio.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { MarcaComponent } from './components/marca/marca.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+// Import the AppRoutingModule
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NosotrosComponent,
+    NavmenuComponent,
+    ClienteComponent,
+    CountsComponent,
+    ServicioComponent,
+    ProductosComponent,
+    MarcaComponent,
+    ContactoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule  // Add AppRoutingModule here
   ],
-  providers: [
-    provideClientHydration()
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
